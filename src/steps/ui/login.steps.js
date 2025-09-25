56f5659f7fad593bbcc33100822ca7bc09a2a29c
Romeo.Gande <Romeo.Gande@endava.com>
2025-09-25 23:32:56 +0300

    small aupdate on login.steps.js

--- src/steps/ui/login.steps.js
+++ src/steps/ui/login.steps.js
@@ -6,8 +6,6 @@ import { expect } from '@playwright/test';
 
 export const { Given, When, Then, Before, After } = createBdd(test);
 
-const loginUrl = process.env.LOGIN_PATH || 'web/index.php/auth/login';
-
 // Given steps
 Given('I am on the OrangeHRM login page', async ({ loginPage, log }) => {
   log.step('Navigate to OrangeHRM login page');
